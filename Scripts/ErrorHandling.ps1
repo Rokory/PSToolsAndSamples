ed8d98b73f8d677d9c7502690c89790990f30acf
Roman Korecky <roman.korecky@easyon.at>
2020-07-29 10:37:46 +0200

    Changed output of errors to warnings.

--- Scripts/ErrorHandling.ps1
+++ Scripts/ErrorHandling.ps1
@@ -3,15 +3,16 @@ $path = 'bla'
 try {
     # This will most likely produce an error
     Remove-Item $path -ErrorAction Stop
+    Write-Host 'This will not be executed, if the file could not be deleted.'
 }
-# catch [System.Management.Automation.ItemNotFoundException] {
-#     Write-Host "Sorry, could not find $path. I cannot delete it."
-# }
+catch [System.Management.Automation.ItemNotFoundException] {
+    Write-Warning "Sorry, could not find $path. I cannot delete it."
+}
 # This will catch all errors
 catch {
     # $PSItem contains the whole error record
     # To obtain the error type, we call GetType() on the Exception property
-    Write-Host $PSItem.Exception.GetType()
+    Write-Warning "Error class: $($PSItem.Exception.GetType())"
     # It is a good idea, to throw the error again
     throw
 }
